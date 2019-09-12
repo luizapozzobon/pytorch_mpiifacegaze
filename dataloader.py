d85db944a122d259912ee1a89a10877f34394175
Luiza <luiza.pozzobon@gmail.com>
2019-09-12 18:25:17 -0300

    new dataloader, but still needs integration with main.py

--- dataloader.py
+++ dataloader.py
@@ -1,98 +1,183 @@
+# -*- coding: utf-8 -*-
+# source: https://github.com/fab-jul/hdf5_dataloader
+
 import os
 import sys
 import h5py
+import glob
+import numbers
+import random
 import numpy as np
-import pandas as pd
-
 import torch
-import torch.utils.data
+from torch.utils.data import Dataset, DataLoader
 
-class MPIIFaceGazeDataset(torch.utils.data.Dataset):
-    def __init__(self, subject_id, dataset_dir):
-        """
-            Load hdf5 file header into memory.
-        """
-        # file names like "p00_0.h5"
-        self.path = os.path.join(dataset_dir, '{}.h5'.format(subject_id))
-        self.dataset = None
-        with h5py.File(self.path, 'r') as file:
-            self.length = len(file["data"])
+default_opener = lambda p_: h5py.File(p_, 'r')
 
-    def __getitem__(self, index):
+class HDF5Dataset(Dataset):
+    def __init__(self, file_ps,
+                 custom_getitem,
+                 files_and_shards,
+                 transform=None,
+                 transform_label=None,
+                 shuffle_shards=True,
+                 opener=default_opener,
+                 seed=42):
         """
-            Load indexed image to memory, then transformations in its shape.
-            Gaze -> #TODO see what returns
+        Parameters:
+            - file_ps: list of file paths to .hdf5 files. Last (alphabetically)
+            file is expected to contain lessimages.
+            - custom_getitem: custom function defined by user to get data from
+            the hdf5 file. It's inputs are be opened hdf5 file and sampled index.
+            - files_and_shards: dictionary containing entries such as
+                    {file1: num_shards1, ..., fileN: num_shardsN}
+            - transform: transformation to apply to read HDF5 sample.
+            - transform_label: tranformation to apply to read HDF5 label.
+            - shuffle_shards: if true, shards are shuffled with seed
         """
-        # https://discuss.pytorch.org/t/dataloader-when-num-worker-0-there-is-bug/25643/16
-        if self.dataset == None:
-            self.dataset = h5py.File(self.path, 'r')
-            self.images = self.dataset['data']
-            self.gazes = self.dataset['label']
 
-        # original shape: [channel[BGR], x, y]
-        # [2, 1, 0] reshapes BGR to RGB [channel[RGB], x, y]
-        # couple of transposes to put image to correct representation
-        # first one puts channel dim last and brings forward the y dim [y, x, channel[RGB]]
-        # second brings forward the x dim and puts y dim second [x, y, channel[RGB]]
-        #img = torch.from_numpy((self.images[index][[2, 1, 0],:,:])).transpose(2, 0).transpose(1, 0)
-        img = torch.from_numpy((self.images[index][[2, 1, 0],:,:])) # expected input [32, 3, 448, 448]
-        #img = torch.unsqueeze(torch.from_numpy((self.images[index][[2, 1, 0],:,:])).transpose(2, 0).transpose(1, 0), 1)
-        gaze = torch.from_numpy(self.gazes[index][0:2])
-        return img, gaze
+        if len(file_ps) == 0 or not all(os.path.isfile(p) for p in file_ps):
+            raise ValueError('Expected list of paths to HDF5 files, got {}'.format(file_ps))
+        self.opener = opener
+        self.ps, self.num_per_shard = HDF5Dataset.filter_smaller_shards(file_ps)
+        if shuffle_shards:
+            r = random.Random(seed)
+            r.shuffle(self.ps)
+        self.transform = transform
+        self.transform_label = transform_label
+
+        ## Custom function defined by user to handle
+        # return of data from hdf5 file.
+        self.custom_getitem = custom_getitem
 
     def __len__(self):
-        return self.length
+        return len(self.ps) * self.num_per_shard
 
-    def __repr__(self):
-        return self.__class__.__name__
+    def __getitem__(self, index):
+        shard_idx = index // self.num_per_shard
+        idx_in_shard = index % self.num_per_shard
+        shard_p = self.ps[shard_idx]
+        with self.opener(shard_p) as f:
+            item = self.custom_getitem(f, idx_in_shard)
+        if self.transform is not None:
+            item['sample'] = self.transform(img)
+        if self.transform_label is not None:
+            item['label'] = self.transform(label)
+        return item
 
+    @staticmethod
+    def filter_smaller_shards(file_ps, opener=default_opener):
+        """
+        Filter away the (alphabetically) last shard, which is assumed to be smaller. This function also double checks
+        that all other shards have the same number of entries.
+        Parameters:
+            - file_ps: list of .hdf5 files, does not have to be sorted.
+            - opener: function to open shards
+        Return:
+            - tuple (ps, num_per_shard), where
+                -> ps = filtered file paths,
+                -> num_per_shard = number of entries in all of the shards in `ps`
+        """
+        assert file_ps, 'No files given'
+        file_ps = sorted(file_ps)  # we assume that smallest shard is at the end
+        num_per_shard_prev = None
+        ps = []
+        for i, p in enumerate(file_ps):
+            num_per_shard = get_num_in_shard(p, files_and_shards, opener)
+            if num_per_shard_prev is None:  # first file
+                num_per_shard_prev = num_per_shard
+                ps.append(p)
+                continue
+            if num_per_shard_prev < num_per_shard:
+                raise ValueError('Expected all shards to have the same number of elements,'
+                                 'except last one. Previous had {} elements, current ({}) has {}!'.format(
+                                    num_per_shard_prev, p, num_per_shard))
+            if num_per_shard_prev > num_per_shard:  # assuming this is the last
+                is_last = i == len(file_ps) - 1
+                if not is_last:
+                    raise ValueError(
+                            'Found shard with too few elements, and it is not the last one! {}\n'
+                            'Last: {}\n'.format(p, file_ps[-1]))
+                print('Filtering shard {}, dropping {} elements...'.format(p, num_per_shard))
+                break  # is last anyways
+            else:  # same numer as before, all good
+                ps.append(p)
+        return ps, num_per_shard_prev
 
-def get_loader(dataset_dir, test_subject_id, batch_size, num_workers, use_gpu):
+def get_num_in_shard(shard_p, files_and_shards, opener=default_opener):
+    hdf5_root = os.path.dirname(shard_p)
+    if files_and_shards[os.path.basename(shard_p)]:
+        num_per_shard = files_and_shards[os.path.basename(shard_p)]
+    else:
+        print('\rOpening {}...'.format(shard_p), end='')
+        with opener(shard_p) as f:
+            num_per_shard = len(f.keys())
+    return num_per_shard
+
+def getitem_func(file, index):
     """
-        Mounts the train and test loaders to all .h5 files from the dataset (15 subjects, 2 files each).
-        Names like: 'p00_0.h5', 'p00_1.h5'
+        Return image and labels from MPIIFaceGaze with correct shapes.
+        PyTorch needs the image to be [channel x width x height].
+        The images came as BGR, so there was a need to invert it
+        to RGB also.
     """
-    assert os.path.exists(dataset_dir)
-    assert test_subject_id in range(15)
+    img = np.transpose(file['data'][index][[2, 1, 0], :, :], (1, 2, 0))
+    label = file['label'][index]
+    return {'sample': img, 'label': label}
 
-    base_subject_ids = ['p{:02}'.format(i) for i in range(15)]
-    base_test_subject_id = base_subject_ids[test_subject_id]
+def get_loaders(files_and_shards, files_path, test_filenames, custom_getitem,
+                extension='.h5', transform=None, transform_label=None,
+                batch_size=16, num_workers=8, use_gpu=True):
+    """
+        Parameters:
+            - files_and_shards: dictionary containing entries such as
+                    {file1: num_shards1, ..., fileN: num_shardsN}.
+            This means it's the filename and the amount of data in it. It's
+            expected the last file to have fewer entries, and the rest have
+            the same amount of data.
+            - files_path: path to folder where files are (in this case, all files
+            are in the same folder, not separated in train/test folders).
+            - test_filenames: which files are to be separated for validation/test.
+            - custom_getitem: custom function defined by user to get data from
+            the hdf5 file. It's inputs are be opened hdf5 file and sampled index.
+            - extension: file extension for hdf5 file ('.h5', '.hdf5', ...)
+            - transform and transform_label: custom data transforms.
+            - batch_size: PyTorch dataloader batch_size.
+            - num_workers: PyTorch's dataloader num_workers param.
+            - use_gpu: PyTorch's dataloader pin_memory param.
+    """
 
-    test_subject_ids = []
-    subject_ids = []
-    for i, subject_id in enumerate(base_subject_ids):
-        for file_part in range(2):
-            subject_ids.append(subject_id + '_' + str(file_part))
+    train_files = glob.glob(files_path + '*' + extension)
 
-    for file_part in range(2):
-        test_subject_ids.append(base_test_subject_id + '_' + str(file_part))
+    test_files = [file for file in train_files if any(f in file for f in test_filenames)]
 
-    train_dataset = torch.utils.data.ConcatDataset([
-        MPIIFaceGazeDataset(subject_id, dataset_dir) for subject_id in subject_ids
-        if subject_id not in test_subject_ids
-    ])
+    train_files = [file for file in train_files if file not in test_files]
 
-    test_dataset = torch.utils.data.ConcatDataset([
-        MPIIFaceGazeDataset(i, dataset_dir) for i in test_subject_ids
-    ])
+    train_dataset = HDF5Dataset(train_files, custom_getitem=getitem_func,
+                 files_and_shards=files_and_shards, transform=transform,
+                 transform_label=transform_label)
 
-    assert len(train_dataset) == 42000
-    assert len(test_dataset) == 3000
+    test_dataset = HDF5Dataset(test_files, custom_getitem=getitem_func,
+                 files_and_shards=files_and_shards, transform=transform,
+                 transform_label=transform_label)
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        pin_memory=use_gpu,
-        drop_last=True,
-    )
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=False,
-        pin_memory=use_gpu,
-        drop_last=False,
-    )
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=num_workers,
+                              pin_memory=use_gpu,
+                              drop_last=True)
+
+    test_loader = DataLoader(test_dataset,
+                             batch_size=batch_size,
+                             shuffle=False,
+                             num_workers=num_workers,
+                             pin_memory=use_gpu,
+                             drop_last=False)
+
+    del train_dataset, test_dataset # let's clear all memory we can lol
+
     return train_loader, test_loader
+
+# train_loader, test_loader = get_loaders(files_and_shards, path, ['p05_0', 'p07_0', 'p12_1'], getitem_func, batch_size=1)
+# files_and_shards = {'p{:02}_{}.h5'.format(i, j):1500 for j in range(2) for i in range(15)}
+
---
+++ transforms.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# https://github.com/fab-jul/hdf5_dataloader/
+
+import torch
+import numbers
+import random
+import numpy as np
+
+class ArrayCenterCrop(object):
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img):
+        """
+        Args:
+            img (np.ndarray): CxHxW array
+        Returns:
+              np.ndarray: cropped array
+        """
+        _, h, w = img.shape
+        th, tw = self.size
+        i = int(round((h - th) / 2.))
+        j = int(round((w - tw) / 2.))
+        return img[:, i:i+th, j:j+tw]
+
+
+class ArrayRandomCrop(object):
+    def __init__(self, size, padding=0, pad_if_needed=False):
+        if padding != 0 or pad_if_needed:
+            # TODO
+            raise NotImplementedError()
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+        self.padding = padding
+        self.pad_if_needed = pad_if_needed
+
+    @staticmethod
+    def get_params(img, output_size):
+        _, h, w = img.shape
+        th, tw = output_size
+        assert h >= th and w >= tw
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return i, j, th, tw
+
+    def __call__(self, img):
+        i, j, h, w = self.get_params(img, self.size)
+
+        return img[:, i:i+h, j:j+w]
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
+
+
+
+class ArrayToTensor(object):
+    def __call__(self, img):
+        return torch.from_numpy(img).float().div(255.)
+
+
+class ArrayRandomHorizontalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return np.flip(img, 2).copy()  # expecting C, H, W
+        return img
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(p={})'.format(self.p)
